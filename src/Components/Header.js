8a701ec975de46f0b74b25d54f55df743aa4f79d
Patricia Eliseu <patyeliseu4@gmail.com>
2022-04-26 11:59:25 -0300

    requisito 04

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 import { getUser } from '../services/userAPI';
 import Loading from './Loading';
 
@@ -33,6 +34,10 @@ class Header extends Component {
             <h3 data-testid="header-user-name">{ usuario }</h3>
           )
         }
+        <Link data-testid="link-to-search" to="/search">search</Link>
+        <Link data-testid="link-to-favorites" to="/favorites">favorites</Link>
+        <Link data-testid="link-to-profile" to="/profile">profile</Link>
+
       </header>
     );
   }
