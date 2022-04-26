952dd519b85408fb25dae4c768dc949d7a45fdd6
Patricia Eliseu <patyeliseu4@gmail.com>
2022-04-26 17:16:27 -0300

    add requisito 05

--- src/pages/Search.js
+++ src/pages/Search.js
@@ -1,14 +1,57 @@
+/* eslint-disable jsx-a11y/control-has-associated-label */
 import React, { Component } from 'react';
 import Header from '../Components/Header';
 
 class Search extends Component {
-  render() {
-    return (
-      <div data-testid="page-search">
-        <Header />
-        <p>search</p>
-      </div>
-    );
+  constructor() {
+    super();
+
+    this.state = {
+      inputNameSearch: '',
+      buttonTrueSearch: true,
+
+    };
   }
+
+    onInput = ({ target }) => {
+      const { name, value } = target;
+      this.setState({ [name]: value }, () => {
+        const { inputNameSearch } = this.state;
+        const numberDois = 2;
+        if (inputNameSearch.length >= numberDois) {
+          this.setState({ buttonTrueSearch: false });
+        } else {
+          this.setState({ buttonTrueSearch: true });
+        }
+      });
+    }
+
+    render() {
+      const { inputNameSearch, buttonTrueSearch } = this.state;
+      return (
+        <div data-testid="page-search">
+          <Header />
+          <form>
+            <label htmlFor="text">
+              <input
+                name="inputNameSearch"
+                type="text"
+                data-testid="search-artist-input"
+                value={ inputNameSearch }
+                onChange={ this.onInput }
+              />
+            </label>
+            <button
+              data-testid="search-artist-button"
+              type="button"
+              disabled={ buttonTrueSearch }
+              onClick={ this.createClick }
+            >
+              Pesquisar
+            </button>
+          </form>
+        </div>
+      );
+    }
 }
 export default Search;
