b8795df944f62a69d4cdb8cfdf2acd60b314ca52
Patricia Eliseu <patyeliseu4@gmail.com>
2022-04-27 09:04:11 -0300

    add requisito 06

--- src/pages/Search.js
+++ src/pages/Search.js
@@ -1,6 +1,9 @@
 /* eslint-disable jsx-a11y/control-has-associated-label */
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 import Header from '../Components/Header';
+import SearchAlbumsAPIs from '../services/searchAlbumsAPI';
+import Loading from '../Components/Loading';
 
 class Search extends Component {
   constructor() {
@@ -9,7 +12,9 @@ class Search extends Component {
     this.state = {
       inputNameSearch: '',
       buttonTrueSearch: true,
-
+      carregaTelaSearch: false,
+      albuns: [],
+      artista: '',
     };
   }
 
@@ -26,30 +31,74 @@ class Search extends Component {
       });
     }
 
+    createClickSearch = async () => {
+      const { inputNameSearch } = this.state;
+      this.setState({
+        carregaTelaSearch: true,
+      });
+      const normal = await SearchAlbumsAPIs(inputNameSearch);
+      const artista = inputNameSearch;
+      console.log('jogo', artista);
+      this.setState({ artista });
+      console.log(normal);
+      this.setState({
+        carregaTelaSearch: false,
+        inputNameSearch: '',
+        albuns: normal,
+      });
+    }
+
     render() {
-      const { inputNameSearch, buttonTrueSearch } = this.state;
+      const { inputNameSearch, buttonTrueSearch,
+        carregaTelaSearch, albuns, artista } = this.state;
       return (
         <div data-testid="page-search">
           <Header />
-          <form>
-            <label htmlFor="text">
-              <input
-                name="inputNameSearch"
-                type="text"
-                data-testid="search-artist-input"
-                value={ inputNameSearch }
-                onChange={ this.onInput }
-              />
-            </label>
-            <button
-              data-testid="search-artist-button"
-              type="button"
-              disabled={ buttonTrueSearch }
-              onClick={ this.createClick }
-            >
-              Pesquisar
-            </button>
-          </form>
+          {
+            carregaTelaSearch ? <Loading /> : (
+              <form>
+                <label htmlFor="text">
+                  <input
+                    name="inputNameSearch"
+                    type="text"
+                    data-testid="search-artist-input"
+                    value={ inputNameSearch }
+                    onChange={ this.onInput }
+                  />
+                </label>
+                <button
+                  data-testid="search-artist-button"
+                  type="button"
+                  disabled={ buttonTrueSearch }
+                  onClick={ this.createClickSearch }
+                >
+                  Pesquisar
+                </button>
+              </form>
+            )
+          }
+          {
+            albuns.length === 0 && <div>Nenhum álbum foi encontrado</div>
+          }
+          <p>
+            { `Resultado de álbuns de: ${artista}` }
+          </p>
+          {
+            albuns.map((elemento, index) => (
+              <div key={ index }>
+                <Link
+                  data-testid={ `link-to-album-${elemento.collectionId}` }
+                  to={ `/album/${elemento.collectionId}` }
+                >
+                  {
+                    elemento.collectionName
+                  }
+                </Link>
+
+              </div>
+            ))
+          }
+          ;
         </div>
       );
     }
